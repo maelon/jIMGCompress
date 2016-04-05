6e4199452c61c27be2c8cea9aea4010eed559086
MaelonJinG <maelon@Maelon.local>
2016-04-05 18:52:41 +0800

    backup

--- src/jIMGCompress.js
+++ src/jIMGCompress.js
@@ -77,5 +77,67 @@
     * 检测文件类型
     */
     scope._checkFileType = (file, callback) => {
+        var fr = new FileReader();
+        fr.addEventListener('load', function (e) {
+            var result = e.target.result;
+
+            //文件头信息判断文件类型，如果直接获取不到文件类型
+            var data = new DataView(e.target.result, 0);
+            var d = [];
+            d.push(data.getUint8(0));
+            d.push(data.getUint8(1));
+            d.push(data.getUint8(2));
+            d.push(data.getUint8(3));
+            d.push(data.getUint8(4));
+            alert(d.join(','));
+            compressIMG(new Blob([e.target.result], {type: 'image/jpeg'})); //type参数与上面判断类型有关
+        });
+        fr.readAsArrayBuffer(file);
     };
+
+                function compressIMG(blob) {
+                try{
+                alert('压缩前大小：' + blob.size/1024 + 'K');
+                var img = document.createElement('img');
+                var url = URL.createObjectURL(blob);
+                img.addEventListener('load', function (e) {
+                    var im = e.target;
+                    URL.revokeObjectURL(url);
+                    var canvas = document.createElement('canvas');
+                    alert(im.naturalWidth + '|' + im.naturalHeight);
+                    canvas.width = 300;
+                    canvas.height = 300 * im.naturalHeight / im.naturalWidth;
+                    var context = canvas.getContext('2d');
+                    context.fillStyle = "#fff";
+                    context.fillRect(0, 0, canvas.width, canvas.height);
+                    context.drawImage(img, 0, 0, canvas.width, canvas.height);
+                    var base64 = canvas.toDataURL('image/jpeg', 0.9);
+                    alert(base64.substring(0, 60));
+                    alert('压缩后大小：' + dataURLtoBlob(base64).size / 1024 + 'K');
+                    previewIMG(base64);
+                });
+                img.src = url;
+                }catch(e) {
+                    alert(e);
+                }
+            }
+
+            function previewIMG(base64) {
+                var img = document.createElement('img');
+                img.addEventListener('load', function (e) {
+                    img.style="position:absolute;top:0;";
+                    document.body.appendChild(img);
+                });
+                img.src = base64;
+            }
+
+            function dataURLtoBlob(dataurl) {
+                var arr = dataurl.split(','), mime = arr[0].match(/:(.*?);/)[1],
+                    bstr = atob(arr[1]), n = bstr.length, u8arr = new Uint8Array(n);
+                while(n--){
+                    u8arr[n] = bstr.charCodeAt(n);
+                }
+                return new Blob([u8arr], {type:mime});
+            }
+
 })(window.jIMGCompress);
